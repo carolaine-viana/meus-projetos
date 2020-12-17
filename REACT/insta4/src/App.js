aeb9323ceeda0f64a9ea8adb270b61c23a0d3f49
Carolaine Viana <carolaine.viana@MacBook-Pro-de-Carolaine.local>
2020-12-17 19:13:35 -0300

    feito

---
+++ REACT/insta4/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import Post from './components/Post/Post';
+//import SavePost from './components/savePost/SavePost;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'app-container'}>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+        <Post
+          nomeUsuario={'Carolaine'}
+          fotoUsuario={'https://picsum.photos/50/50/?blur'}
+          fotoPost={'https://picsum.photos/200/150/?blur=2'}
+        />
+        <Post
+          nomeUsuario={'Joselindo'}
+          fotoUsuario={'https://picsum.photos/50/50.jpg'}
+          fotoPost={'https://picsum.photos/seed/picsum/200/150'}
+        />
+        <Post
+          nomeUsuario={'Douglas Manson'}
+          fotoUsuario={'https://picsum.photos/id/237/200/300'}
+          fotoPost={'https://picsum.photos/id/237/200/150'}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ REACT/insta4/src/components/savePost/SavePost.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import'./SavePost.css'
+
+export function savePost(props){
+    <h1>TESTE</h1>
+}
+
